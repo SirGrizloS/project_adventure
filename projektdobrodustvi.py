66488b7ee4038ca2370d3ba188c5201414329803
dejvhydos <74410749+SirGrizloS@users.noreply.github.com>
2024-06-06 11:26:12 +0200

    Update projektdobrodustvi.py
    Another update in FIGHT (enemy attack now, eval added - end of definition, "random enemy" values added, fixed some mistakes in fight)

--- projektdobrodustvi.py
+++ projektdobrodustvi.py
@@ -8,7 +8,7 @@ pochoden = 3
 
 
 def fight():
-    global random_final, random_damage, enemy_hp
+    global random_final, random_damage, enemy_hp, hp
     print("Zaútočili na vás!")
     enemy_hp = 100
     while True:
@@ -18,16 +18,24 @@ def fight():
             print("Prohrál jste!")
         
         random_value = randrange(0, 100)
+        random_value_enemy = randrange (0, 100)
         random_calculate = random_value % 2
+        random_calculate_enemy = random_value_enemy %2
         if random_calculate == 1:
             random_final = True
         else:
             random_final = False
+        if random_calculate_enemy == 1:
+            random_final_enemy = True
+        else:
+            random_final_enemy = False
         random_damage = randrange(15, 30)
-        try:
+        random_damage_enemy = randrange(10, 20)
+        try:        
             print("1. Útok")
             print("2. Krytí")
             respond = int(input("Zvolte možnost. (1/2):   "))
+            
             if respond == 1:
                 print("Pokusil jste se zaútočit na nepřítele!")
                 if random_final == True:
@@ -39,13 +47,40 @@ def fight():
                             print(f"Nepřítel má nyní {enemy_hp}HP")
                         elif enemy_hp <= 0:
                             print("Zneškodnil jste nepřítele!")
-                        print(f"Máte {hp}HP!")
+                            eval(return_function + "()")
                     else:
                         print(f"Udělil jste poškození za {random_damage}HP!")
-                        print(f"Nepřítel má nyní {enemy_hp}HP")
-                        print(f"Máte {hp}HP!")
+                        if enemy_hp > 0:
+                            print(f"Nepřítel má nyní {enemy_hp}HP")
+                        elif enemy_hp <= 0:
+                            print("Zneškodnil jste nepřítele!")
+                            eval(return_function + "()")
+                    if random_final_enemy == True:
+                        print(f"Nepřítel vám udělil poškození za {random_damage_enemy}HP!")
+                        hp = hp - random_damage_enemy
+                        check_stats()
+                    else:
+                        print("Nepřítel minul. Jste na tahu!")
                 elif random_final == False:
                     print("Minul jste se!")
+                    if random_final_enemy == True:
+                        print(f"Nepřítel vám udělil poškození za {random_damage_enemy}HP!")
+                        hp = hp - random_damage_enemy
+                        check_stats()
+                    else:
+                        print("Nepřítel minul. Jste na tahu!")
+            elif respond == 2:
+                print("Schoval jste se za nejbližší kryt.")
+                if random_final == True:
+                    print("Nepřítel vás netrefil!")
+                elif random_final == False:
+                    if random_final_enemy == True:
+                        print(f"Nepřítel vám udělil poškození za {random_damage_enemy}HP!")
+                        hp = hp - random_damage_enemy
+                        check_stats()
+                    else:
+                        print("Nepřítel minul. Jste na tahu!")
+                
         except ValueError:
             print("Napište 1, nebo 2!")
 
